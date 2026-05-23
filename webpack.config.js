704430bcb09871b7ee6e35f98564eab3774e7110
MintCat98 <presidentmc9898@gmail.com>
2026-05-23 17:53:18 +0900

    build: project init setting

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    background: './src/background/index.ts',
+    content:    './src/content/index.ts',
+    popup:      './src/popup/Popup.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@content': path.resolve(__dirname, 'src/content'),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/popup/index.html',
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '.' },
+        { from: 'src/assets', to: 'assets' },
+      ],
+    }),
+  ],
+};
