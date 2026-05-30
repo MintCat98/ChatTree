f66ef3d5a47d492eb2acf95139f2f0ff9615da6a
ignisytb <dg.yangtaebin@gmail.com>
2026-05-31 02:28:58 +0900

    docs: remove useless comments from scrollToNode

--- src/content/scroll-navigator.ts
+++ src/content/scroll-navigator.ts
@@ -3,7 +3,6 @@
 import { SELECTORS } from '../shared/constants';
 
 export function scrollToNode(navId: string): void {
-  // select element from navId attribute
   const el = document.querySelector(`[${SELECTORS.NAV_ID_ATTR}="${navId}"]`);
 
   if (!el) return;
@@ -13,7 +12,6 @@ export function scrollToNode(navId: string): void {
     block: 'center',
   });
 
-  // add highlight class for 1.5s to indicate
   el.classList.add('nav-highlight');
   setTimeout(() => el.classList.remove('nav-highlight'), 1500);
 }
